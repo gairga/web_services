58815f8135a14b0e94e5008124abab37ea8eaee7
Gabriela Ilarreta <gabriela@gmail.com>
2016-09-20 17:48:44 -0300

    Creación de CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   resources :users
+  #resources :users, only: [:index, :create, :destroy, :update, :show, :edit]
+  root 'users#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
